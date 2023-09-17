f7f0379694458ae8568af1f540e0e3029590ac10
Okafor Patrick <patrickpeters911@gmail.com>
2023-09-17 13:51:31 -0700

    Handle PATH

--- s_execute.c
+++ s_execute.c
@@ -12,25 +12,63 @@
 void execute_command(char *command, char **arguments, char **env)
 {
 	pid_t pid;
+	int status;
+	char *path;
+	char *path_copy;
+	char *path_token;
+	char *command_path;
+
+	command_path = NULL;
+
+	if (command[0] == '/')
+	{
+		if (access(command, F_OK) == 0)
+			command_path = strdup(command);
+	}
+	else
+	{
+		path = getenv("PATH");
+		path_copy = strdup(path);
+		path_token = strtok(path_copy, ":");
+
+		while (path_token != NULL)
+		{
+			command_path = strdup(path_token);
+			command_path = _strcat(command_path, "/");
+			command_path = _strcat(command_path, command);
+			if (access(command_path, F_OK) == 0)
+			break;
+		free(command_path);
+		command_path = NULL;
+		path_token = strtok(NULL, ":");
+		}
+
+		free(path_copy);
+	}
+
+	if (command_path == NULL)
+	{
+		_printf("%s: command not found\n", command);
+		return;
+	}
 
 	pid = fork();
 	/* Execute the command using execve */
 	if (pid == 0)
 	{
-		execve(command, arguments, env);
-		perror(command);
+		execve(command_path, arguments, env);
+		perror(command_path);
 		exit(1);
 	}
 	/*Parent process*/
 	else if (pid > 0)
-	{
-		int status;
 
 		waitpid(pid, &status, 0);
-	}
 	else
 	{
 		perror("forking error");
 		exit(1);
 	}
+
+	free(command_path);
 }
